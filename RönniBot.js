625902d8c63f619cd954ec933cea930d96479f63
Elias <62937543+oveeauki@users.noreply.github.com>
2021-07-20 17:23:44 +0300

    Early version of discord.js bot

--- RönniBot.js
+++ RönniBot.js
@@ -4,10 +4,13 @@ const client = new Discord.Client();
 const configit = require('./Config/rönniconfiguraatio.json');
 const oneliners = require('./Config/fraasit.json');
 const emojit = require('./Config/emojit.json');
+const moment = require('moment');
 const {colors} = require('./omatmoduulit/escapecolors');
+const {bogosembed} = require('./omatmoduulit/bogos')
+var time = moment().format('LTS')
 var {Randomi} = require('./omatmoduulit/Random');
-/***********************************************************************************/
 
+/***********************************************************************************/
 client.once('ready',async() => {
 await client.user.setStatus("dnd");
 await client.user.setActivity(oneliners.fraasit[Randomi],{type:"WATCHING"});
@@ -20,46 +23,51 @@ console.log(
 'Rönni sanoo : \033[34m['+ oneliners.fraasit[Randomi]+']\n\033[0m'+
 '----------------------------------------------------------------');
 });
-
 /************************************************************************************/
 
 client.on('message', async message => {
-if(message.content === 'millanen chillailu?'){
-message.channel.send('tälläne chillailu ! <@189443359998214145> ',{
-files:[
-'https://cdn.discordapp.com/attachments/343890302781620226/863565315736207370/unknown.png'
-]
-}).then(console.log).catch(console.error);
+if(message.content === 'bogos'){
+const msf = message.author.username
+message.reply(bogosembed)
+console.log(`${client.user.username} answered ${msf} @${time}`)
 }
 
-if(message.channel.type === 'dm' && message.author.id === '300648311067508754'){
+if(message.content.startsWith('poista') && message.author.id === '300648311067508754'){
+const int = message.content.match(/\d+/)
+const intparsed = parseInt(int)
+message.channel.bulkDelete(intparsed)
+message.reply(`Mää pistin meneen ${intparsed} viesti eestä !`)
+message.channel.send('<:tita2:814269332829503498>')
+console.log(`${client.user.username} Deleted [${intparsed}] messages @ ${message.channel.name}`)
+}
+else if(message.content.startsWith('poista') && message.author.id != '300648311067508754'){
+message.reply(`Niin mitä ihmettäää? Vähän OutoOoO kun et voin käyttää tätä vai mitä ?\n`,bogos.bogosembed)
+}
+/* Logailua */
+if(message.channel.type === 'dm'){
+console.log(`${message.author.username} Sent a message : [${message.content}] @ ${time}`)
+}
+else if(message.channel.type === 'dm' && message.author.id === '189443359998214145'){
 const lastmsgusr = message.channel.lastMessage.author.username
 console.log(`${message.author.username} Sent a message : [${message.content}]`)
-await client.users.cache.get(`${message.channel.lastMessage.author.id}`).send(oneliners.fraasit[Math.floor(Math.random()*6)])
+await client.users.cache.get(`${message.channel.lastMessage.author.id}`).send(oneliners.fraasit[Math.floor
+(Math.random()*6)])
 console.log(`Röndelssi Sent [${client.user.lastMessage.content}] To [${lastmsgusr}]`)      
+}
+else if(message.channel.type ==='text' && message.channel.id != '672891512240996409'){
+console.log(`${message.author.username} @ ${message.channel.name} Sent : [${message.channel.lastMessage.content}]`)
 }
 
 if(message.content === 'jeee' && message.author.id === '300648311067508754'){
-const fraasit = {
-0:"Tämä on A",
-1:"Tämä on B",
-2:"Tämä on C"
-};
-for(var i=0;i<3;i++){
-message.channel.send(fraasit[Math.floor(Math.random()*3)])
-}
 message.channel.send(emojit.aaro)
 }
-
-if(message.content === 'moi rönni' && message.author.id == '300648311067508754'){
-for(var i =0;i<5;i++){
-(await client.users.fetch('300648311067508754')).send('staattinenpitkätupla')
-}
+else if(message.content === 'moi' && message.author.id == '300648311067508754'){
+(await client.users.fetch('237577351238516738')).send(bogosembed)
 }
 
 // Joinaaa
 if (message.content === 'rönnisummonaus') {
-const yheteys = await message.member.voice.channel.join();
+await message.member.voice.channel.join();
 console.log('Rönni joinas kanavalle : ['+message.member.voice.channel.name+']')
 }
 // Leavaaaaa
@@ -68,13 +76,7 @@ message.member.voice.channel.leave();
 console.log('Rönni leavas kanavalta : ['+message.member.voice.channel.name+']')
 }
 
-/*@self Reaktio
-if(message.author === client.users.cache.get('300648311067508754')){
-const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'aaro');
-message.react(reactionEmoji).then(console.log('Rönni reagoi\n~',message.content))
-}
-*/
-
+/************************************************************************************/ 
 if(message.content === 'resetnick'){
 await client.user.setUsername('Rönni')
 console.log('Username resetattu takas : ['+client.user.username+']')
@@ -90,11 +92,12 @@ message.channel.send('Nyt oon kanko!\nNäin.')
 console.log("Rönnin Nickki vaihtu nickkiin : ["+client.user.username+"]")
 }
 
-/************************************************************************************/ 
-
-if(message.content === '2'){
-console.log(Number.isInteger(a))
+/*Reaktio
+if(message.author === client.users.cache.get('300648311067508754')){
+const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'aaro');
+message.react(reactionEmoji).then(console.log('Rönni reagoi\n~',message.content))
 }
+*/
 });
 
-client.login(configit.token);
+client.login(configit.token)
