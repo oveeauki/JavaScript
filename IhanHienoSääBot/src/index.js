84840cb5404cf93d94e55f969a498bd41f4dd69a
FreDi <62937543+oveeauki@users.noreply.github.com>
2022-03-10 23:22:24 +0200

    ⠀

--- IhanHienoSääBot/src/index.js
+++ IhanHienoSääBot/src/index.js
@@ -27,6 +27,16 @@ Bot Username [${client.user.username}]
 });
 
 client.on("message",async message => {
+let begin = message.content.split(" ");
+if(message.channel.type == "dm" && !message.author.bot){
+console.log(`[${message.author.username}] Sent : ${message.content}`)
+message.channel.startTyping();
+setTimeout(function ok(){
+message.reply(`**Usage: Enter $weather <Location> 
+at any channel in a server which the bot is part of.**`);
+},2000);
+message.channel.stopTyping();
+}
 if(message.content.startsWith("delete") && message.author.id === '300648311067508754'){
 const int = message.content.match(/\d+/);
 const parsed = parseInt(int);
@@ -35,44 +45,46 @@ message.reply(`I deleted [${parsed}] message\\s !`).then(msg => {
 msg.delete({ timeout: 9000 })});
 console.log(`Deleted [${parsed}] Message\\s @ ${message.guild.name} (${message.channel.name})`);
 }
-let begin = message.content.split(" ");
 if(message.channel.type == 'text' && begin[0] == '$weather' && !message.author.bot){
-let _mssg = message.channel.id;
+let _Ch_ID = message.channel.id;
 let kaupunki = message.content;
 let choice = kaupunki.split(" ").pop();
 let url = `http://api.weatherapi.com/v1/current.json?key=${Weather_API_t}&q={${choice}}&aqi=no`
 
 request(url,(err,res,body) => {
 
-let message = JSON.parse(body);
-let city = message.location.name, country = message.location.country;
-let region = message.location.region, time = message.location.localtime;
-let C = message.current.temp_c, F = message.current.temp_f;
-let feels_c = message.current.feelslike_c, feels_f = message.current.feelslike_f;
-let wind_D = message.current.wind_dir; 
-let visikm = message.current.vis_km, visimiles = message.current.vis_miles;
-let booleandayxd = message.current.is_day, humidity = message.current.humidity;
-let uv = message.current.uv, atmosmb = message.current.pressure_mb;
+let __message = JSON.parse(body);
+let city = __message.location.name, country = __message.location.country;
+let region = __message.location.region, time = __message.location.localtime,timesplitted = time.split(" ");
+let lat = __message.location.lat,lon = __message.location.lon;
+let C = __message.current.temp_c, F = __message.current.temp_f;
+let feels_c = __message.current.feelslike_c, feels_f = __message.current.feelslike_f;
+let wind_D = __message.current.wind_dir; 
+let visikm = __message.current.vis_km, visimiles = __message.current.vis_miles;
+let booleandayxd = __message.current.is_day, humidity = __message.current.humidity;
+let uv = __message.current.uv, atmosmb = __message.current.pressure_mb;
 
-let _message_ = [
-`Country [${country}]
+let _message_ = 
+[`Country [${country}]
 City [${city}]
 Region [${region}]
-Local Time[${time}]
-Atmosphere Pressure [${atmosmb}] Mbar
+Date [${timesplitted[0]}]
+Local Time (${timesplitted[1]})
+Latitude (${lat})° | Longitude (${lon})°
+Atmosphere Pressure (${atmosmb}) Mbar
 Temperature (${C})°C | (${F})°F
 Feels Like (${feels_c})°C | (${feels_f})°F
-Wind Direction [${wind_D}]
-Visibility (KM) [${visikm}] | (Miles) [${visimiles}]
-Is_day Boolean xd  [${booleandayxd}]
+Wind Direction (${wind_D})
+Visibility (KM) (${visikm}) | (Miles) (${visimiles})
+Is_day Boolean xd  (${booleandayxd})
 Humidity (${humidity})%
-UV Index [${uv}]
-`];
+UV Index (${uv})`];
+
 if(err)
     console.log(`Tällänen Virhe xd [${err}]`);
 
 else if(res.statusCode == 200 && !err){
-    client.channels.cache.get(`${_mssg}`).send(`**${_message_}**`);
+    client.channels.cache.get(`${_Ch_ID}`).send(`**${_message_}**`);
 }
 });
 }
