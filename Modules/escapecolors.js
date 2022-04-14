778707fb36bb0b40dc4fe1a2833809680ddc617c
FreDi <62937543+oveeauki@users.noreply.github.com>
2022-04-14 19:01:10 +0300

    Update escapecolors.js

--- Modules/escapecolors.js
+++ Modules/escapecolors.js
@@ -8,7 +8,6 @@ const colors = {
     magenta: '\033[35m',
     cyan: '\033[36m',
     white: '\033[37m',
-
     blackBg: '\033[40m',
     redBg: '\033[41m',
     greenBg: '\033[42m',
@@ -16,6 +15,14 @@ const colors = {
     blueBg: '\033[44m',
     magentaBg: '\033[45m',
     cyanBg: '\033[46m',
-    whiteBg: '\033[47m'
+    whiteBg: '\033[47m',
+    reset: "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    underscore: "\x1b[4m",
+    blink: "\x1b[5m",
+    reverse: "\x1b[7m",
+    hidden: "\x1b[8m",
 }
-module.exports = {colors}
+
+module.exports = {colors};
