fe45fc9ac9e6ac97378043646b59f58f107f7859
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2022-07-25 15:56:05 +0300

    Update Array_pop()_Sample.js

--- Demos/Array_pop()_Sample.js
+++ Demos/Array_pop()_Sample.js
@@ -1,6 +1,6 @@
 /**   
 @auth oveeauki
-@desc Array.pop() Sample
+@desc Array.pop(); Sample
                         */
 const {stdout,stdin,exit} = require("process");
 const rl = require("readline").createInterface({
@@ -8,35 +8,40 @@ input:stdin,
 output:stdout
 });
 
-let finish = (array = []) => {
-const arraysize = array.length;
-stdout.write(`\n\nArray is complete empty(${arraysize}) ${array} \n`);
+class arraythings { 
+constructor(arr = []){
+this.arr = arr;
+}
+
+finish = (arr = []) => {
+this.arraysize = this.arr.length;
+stdout.write(`\n\nArray is complete empty(${this.arraysize}) ${arr}\n`);
 setTimeout(() => {
 exit(0);
 },3000);
 };
 
-var emptyarray = async(array = []) => {
+async emptyarray(array = []){
 console.clear();
 stdout.write("Array Includes : ");
 for(var a in array){
 stdout.write(`${array[a]} `);
 }
-for(let i=array.length;i>=1;i--){
+for(let i=array.length-1;i>=0;i--){
 await new Promise((res) => {
 setTimeout(() => {
 try{
+console.log(`\n\nRemoving array obj #(${i}) | ${array[i]}\nNow the Array includes ${array} `);
 array.pop(i);
 res();
 }catch{Error}
-stdout.write(`\n\nRemoving array obj [${(i)}]\nNow the Array includes [${array}] `)
 },1500);
 });
 }
-finish(array);
-};
+this.finish(array);
+}
 
-let fillarray = async(amount) => {
+async fillarray(amount){
 console.clear();
 let arraunum = [];
 for(var i=0;i<amount;i++){
@@ -52,14 +57,15 @@ for(let objs in arraunum){
 stdout.write(`${arraunum[objs]} `) 
 }
 try{
-emptyarray(arraunum)
+this.emptyarray(arraunum)
 }catch{Error};
 return(0);
-};
+}};
 
 let _Main = () => {
 rl.question(`Amount of numbers to enter in Array?\n\u279C `,(amount) => {
-fillarray(amount);
+let _init = new arraythings();
+_init.fillarray(amount);
 });
 };
 
