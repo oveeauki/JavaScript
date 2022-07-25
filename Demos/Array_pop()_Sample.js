937c483751dec952b12446e45893247419c948e9
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2022-07-25 16:45:57 +0300

    ⠀

--- Demos/Array_pop()_Sample.js
+++ Demos/Array_pop()_Sample.js
@@ -1,7 +1,7 @@
 /**   
 @auth oveeauki
 @desc Array.pop(); Sample
-                        */
+                          */
 const {stdout,stdin,exit} = require("process");
 const rl = require("readline").createInterface({
 input:stdin,
@@ -12,10 +12,10 @@ class arraythings {
 constructor(arr = []){
 this.arr = arr;
 }
-
+ 
 finish = (arr = []) => {
 this.arraysize = this.arr.length;
-stdout.write(`\n\nArray is complete empty(${this.arraysize}) ${arr}\n`);
+console.log("\n\nArray is complete empty %s\n",arr);
 setTimeout(() => {
 exit(0);
 },3000);
@@ -31,7 +31,7 @@ for(let i=array.length-1;i>=0;i--){
 await new Promise((res) => {
 setTimeout(() => {
 try{
-console.log(`\n\nRemoving array obj #(${i}) | ${array[i]}\nNow the Array includes ${array} `);
+console.log(`\n\nRemoving array obj #(${i+1}) | ${array[i]}\nNow the Array includes: [${array}] `);
 array.pop(i);
 res();
 }catch{Error}
@@ -46,7 +46,7 @@ console.clear();
 let arraunum = [];
 for(var i=0;i<amount;i++){
 await new Promise((res) => {
-rl.question(`Enter Number ${(i+1)}\n\u279C `,(dat) => {
+rl.question(`Enter Char ${(i+1)}\n\u279C `,(dat) => {
 arraunum[i] = dat;
 res();
 });
@@ -63,9 +63,19 @@ return(0);
 }};
 
 let _Main = () => {
-rl.question(`Amount of numbers to enter in Array?\n\u279C `,(amount) => {
+rl.question(`Amount of chars to enter in Array...\n\u279C `,(amount) => {
 let _init = new arraythings();
+if(amount > 12){
+   stdout.write("No need to input over dozen objects...\nits just a test\n");
+   exit(1);
+}
+else if(/\d+/.test(amount) == 0){
+   stdout.write("Enter a Number...\n");
+   exit(1);
+}
+else{
 _init.fillarray(amount);
+}
 });
 };
 
