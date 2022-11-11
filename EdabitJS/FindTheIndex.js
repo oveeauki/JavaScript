a5bfaf73f293aff0dab4dfd4dca2fec312cb2535
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2022-11-11 09:02:49 +0200

    Update FindTheIndex.js

--- EdabitJS/FindTheIndex.js
+++ EdabitJS/FindTheIndex.js
@@ -5,17 +5,18 @@
 const {exit} = require("process");
 
 let _main = async(array = [],int) => {
+console.clear();
 var resultarray = [];
 for(let i=0;i<array.length;i++){
-await new Promise((res) => {
+console.log("Indexing Currently: [%s]\nFound: [%s]\nTimes: [%d]",array[i],resultarray,resultarray.length); 
+  await new Promise((res) => {
     if(array[i] == int){
-        resultarray.push(array[i]);
-        console.clear();  
-        console.log("Found: [%s]\nTimes: [%d]",resultarray,resultarray.length); 
+      resultarray.push(array[i]);
 }
       setTimeout(() => {
-        res();
-      },1500);
+        console.clear();
+          res();
+          },1500);
 });
 }
 exit(0);
