8ec420855a93c6ef680e6ecedd155c126be0643c
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2022-12-01 20:11:38 +0200

    Update modulusremainder.js

--- Demos/modulusremainder.js
+++ Demos/modulusremainder.js
@@ -1,29 +1,30 @@
-/*
-    Return remainder of 2 int's
-                                 */
+/**
+  *@auth 0xFreDi
+  *@Desc Return remainder of 2 values
+                                      */
 const {stdout,stdin,exit} = require("process");
 const rl = require("readline").createInterface({
 input:stdin,
 output:stdout  
 });
 
-let solve = (array = []) => {
+let remain = (array = []) => {
 const remainder = (array[0]%array[1]);
-stdout.write(`Remainder of numbers : [${array[0]}] & [${array[1]}] = ${remainder}\n`);
-exit(0);
-};
+return(remainder);
+}
 
 let _init = async() => {
 let numbers = [];
-for(let i=0;i<2;i++){
-await new Promise((resolve) => {
-rl.question(`Enter number ${(i+1)}\n\u279C `,(int) => {
-numbers[i] = int;
-resolve();
+  for(let i=0;i<2;i++){
+    await new Promise((res) => {
+      rl.question(`Enter number ${(i+1)}\n\u279C `,(int) => {
+      numbers[i] = int;
+      res();
 });
 });
 }
-solve(numbers);
-};
+console.log("Remainder of [%d] & [%d] is: [%d]",numbers[0],numbers[1],remain(numbers));
+exit(0);
+}
 
 _init();
