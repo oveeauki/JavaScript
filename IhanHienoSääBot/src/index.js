6c0a1739de27630e999522c21e3051080658d1da
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2024-06-27 19:13:26 +0300

    Update index.js

--- IhanHienoSääBot/src/index.js
+++ IhanHienoSääBot/src/index.js
@@ -1,21 +1,20 @@
 /**
 @author 0xFreDi
 @description HienoSääBotti Main
-@todo add chunk splitter bc of 
-message max length in discord api
-                                  **/
+@todo add chunk splitter as 2nd optional condition 
+bc of message max length in discord api
+                                                **/
 import {OpenAI} from "openai"
 import * as Discord from "discord.js"
 import cf from "../Config/config.json" assert {type:"json"}
 import {stdin,stdout,exit} from "process"
 import axios from "axios"
 import claude from "@anthropic-ai/sdk"
-import {AIAPI,claudeaiapi} from "../Modules/apis.js"
+import {AIAPI,claudeaiapi,weatherapi} from "../Modules/apis.js" 
 import crypto from "node:crypto" 
 
 const aicli = new OpenAI({apiKey:cf.OpenAI_t});
 const client = new Discord.Client();
-const Weather_API_t = cf.Weatherapi_t;
 const _claudeai = new claude({apiKey:cf.claudeai_t});
 
 var prfx = "!";
@@ -71,7 +70,7 @@ client.on("message",async(message) => {
   const opts = ["gpt4","dalle","hash","claude"]
   let viesti = message.content.split(" ")
   const myid = "300648311067508754";
-
+  const choice = msgfinal.replace(/^(w\s+|weather\s*)/i,'').trim();
 /*--------------------------------Message Channel BulkDelete----------------------------------------*/
   if((message.content.startsWith(".") && msgfinal.includes("delete")) && message.author.id == myid){
     try{
@@ -157,75 +156,20 @@ client.on("message",async(message) => {
       await message.reply(`hash of ${hssplit[1]} in ${hssplit[0]} is [${hash_.digest("hex")}]`);
   }catch{Error};
   }
+
 /* -------------------------- Weather ----------------------------------- */
   if((viesti[0] == ".w" && !message.author.bot) || (message.content.startsWith(".") && msgfinal.includes("weather"))){
-    try{
-        let __message;
-        const choice = msgfinal.replace(/^(w\s+|weather\s*)/i,'').trim();
-        const encodedChoice = encodeURIComponent(choice);
-        let url = `http://api.weatherapi.com/v1/current.json?key=${Weather_API_t}&q={${encodedChoice}}&aqi=no`
-        const fetc = await axios.get(url).then(dat => {__message = dat.data});
-        let city = __message.location.name, country = __message.location.country;
-        let region = __message.location.region, time = __message.location.localtime,timesplitted = time.split(" ");
-        let lat = __message.location.lat,lon = __message.location.lon;
-        let C = __message.current.temp_c, F = __message.current.temp_f;
-        let feels_c = __message.current.feelslike_c, feels_f = __message.current.feelslike_f;
-        let wind_D = __message.current.wind_dir; 
-        let visikm = __message.current.vis_km, visimiles = __message.current.vis_miles;
-        let booleandayxd = __message.current.is_day, humidity = __message.current.humidity;
-        let uv = __message.current.uv, atmosmb = __message.current.pressure_mb;
-        let windkm = __message.current.gust_kph, windmph = __message.current.gust_mph
-        let condition = __message.current.condition.text
-
-        if(booleandayxd){
-          booleandayxd = true;
-        }
-        else{
-          booleandayxd = false;
-        }
-        const embed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(city)
-        .addField('\u2022 Date',timesplitted[0],true)
-        .addField('\u2022 Time (Local)',timesplitted[1],true)
-        .addField('\u2022 Country',country)
-        .addField('\u2022 Temperature',`(${C})°C  |  (${F})°F`,true)
-        .addField('\u2022 Condition',condition,true)
-        .addField('\u2022 Feels Like',`(${feels_c})°C | (${feels_f})°F`)
-        .addField('\u2022 Region',region,true)
-        .addField('\u2022 Visibility',`(${visikm})Km |(${visimiles}) Miles `,true)
-        .addField('\u2022 Cordinates',`Latitude (${lat})° | Longitude(${lon})°`)
-        .addField('\u2022 Wind Direction',wind_D,true)
-        .addField('\u2022 Wind Speed',`(${windkm}) Km/h | (${windmph}) Mph`,true)
-        .addField('\u2022 Humidity',`${humidity}%`)
-        .addField('\u2022 UV Index',uv,true)
-        .addField('\u2022 Is_Day Boolean xd',booleandayxd,true);
-        await message.reply(embed);
-        }catch{Error};
-  }
-/*
-  else if(viesti[0] == "." && viesti[1].toLowerCase() == "forecast" && !message.author.bot){
   try{
-    let __message;hasha
-      const choice = viesti[2].toLowerCase();
-      const days = viesti[3].toLowerCase();
-
-      let url = `http://api.weatherapi.com/v1/forecast.json?key=${Weather_API_t}&q={${choice}}&days=${days}`
-      const fetc = await axios.get(url).then(dat => {__message = dat.data});
-      console.log(__message.is_day[0]);
-
-      let city = __message.location.name,country = __message.location.country;
-      let region = __message.location.region;
-
-  let _message__ = [
-  `\u27AA Country [${country}]
-  \u27AA City [${city}]
-  \u27AA Region [${region}]
-
-  `];
-      const fin = `\`\`\`\n${_message__}\n\`\`\``;
-      await message.reply(fin);
-  }catch{Error};
+    const w_api = new weatherapi();
+    await w_api.w_current(choice);
+    await message.reply(w_api.res)
+    }catch{Error}
   }
-*/
+  else if((viesti[0] == ".f" && !message.author.bot) || (message.content.startsWith(".") && msgfinal.includes("forecast"))){
+    try{
+    const w_api = new weatherapi();
+    await w_api.w_fech(choice);
+    await message.reply(w_api.emb);
+  }catch{Error}
+}
 });
