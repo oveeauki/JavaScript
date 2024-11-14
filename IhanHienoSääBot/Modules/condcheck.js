7dc6f88d3db4a673359530ab24a6a5b642465c1d
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2024-11-14 04:18:12 +0200

    Update condcheck.js

--- IhanHienoSääBot/Modules/condcheck.js
+++ IhanHienoSääBot/Modules/condcheck.js
@@ -1,24 +1,26 @@
-// Weather Condition Emojis Handle
-// TODO: change the API to some other less ridiculous one that has no duplicate conditions and fewer in general...
+//condcheck
 export function condcheck(cond){
-  const we = {
-  "Light drizzle":"🌧️",
-  "Light rain shower":"🌧️",
-  "Partly Cloudy ":"⛅",
-  "Partly cloudy":"⛅",
-  "Overcast":"⛅",
-  "Sunny":"☀️",
-  "Clear":"☀️",
-  "Cloudy":"☁️",
-  "Rain":"🌧️",
-  "Moderate rain":"🌧️",
-  "Heavy rain":"🌧️🌧️",
-  "Patchy rain nearby":"🌦️",
-  "Patchy light drizzle":":🌦️",
-  "Light rain":"🌦️",
-  "Snow":"❄️"
-}
+  const jo = cond.trim();
+	  const we = {
+		    "Light drizzle":"🌧️",
+		    "Light rain shower":"🌧️",
+		    "Partly Cloudy":"⛅",
+		    "Partly cloudy":"⛅",
+		    "Overcast":"⛅",
+		    "Sunny":"☀️",
+		    "Clear":"☀️",
+		    "Cloudy":"☁️",
+		    "Rain":"🌧️",
+		    "Moderate rain":"🌧️",
+		    "Heavy rain":"🌧️🌧️",
+		    "Patchy rain nearby":"🌦️",
+		    "Patchy light drizzle":":🌦️",
+		    "Light rain":"🌦️",
+		    "Snow":"❄️",
+        "Fog":"🌫️",
+        "Mist":"🌫️"
+	  }
 
-const conditionEmoji = we[cond];
-return(conditionEmoji)
+	const conditionEmoji = we[jo];
+	return(conditionEmoji)
 }
