c6d1679ea8a13dbc2a568951e41ecd5474826945
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2024-11-19 14:58:23 +0200

    Update condcheck.js

--- IhanHienoSääBot/Modules/condcheck.js
+++ IhanHienoSääBot/Modules/condcheck.js
@@ -2,24 +2,29 @@
 export function condcheck(cond){
   const jo = cond.trim();
 	  const we = {
-		"Light drizzle":"🌧️",
-		"Light rain shower":"🌧️",
-		"Partly Cloudy":"⛅",
-		"Partly cloudy":"⛅",
-		"Overcast":"⛅",
-		"Sunny":"☀️",
-		"Clear":"☀️",
-		"Cloudy":"☁️",
-		"Rain":"🌧️",
-		"Moderate rain":"🌧️",
-		"Heavy rain":"🌧️🌧️",
-		"Patchy rain nearby":"🌦️",
-		"Patchy light drizzle":":🌦️",
-		"Light rain":"🌦️",
-		"Snow":"❄️",
-        	"Fog":"🌫️",
-        	"Mist":"🌫️"
+		    "Light drizzle":"🌧️",
+		    "Light rain shower":"🌧️",
+		    "Partly Cloudy":"⛅",
+		    "Partly cloudy":"⛅",
+		    "Overcast":"⛅",
+		    "Sunny":"☀️",
+		    "Clear":"☀️",
+		    "Cloudy":"☁️",
+		    "Rain":"🌧️",
+		    "Moderate rain":"🌧️",
+		    "Heavy rain":"🌧️🌧️",
+		    "Patchy rain nearby":"🌦️",
+		    "Patchy light drizzle":":🌦️",
+		    "Light rain":"🌦️",
+		    "Snow":"❄️",
+        "Blizzard":"🌨️",
+        "Moderate or heavy snow showers":"🌨️",
+        "Light snow":"🌨️",
+			  "Heavy snow":"🌨️🌨️",
+        "Fog":"🌫️",
+        "Mist":"🌫️"
 	  }
+
 	const conditionEmoji = we[jo];
 	return(conditionEmoji)
 }
