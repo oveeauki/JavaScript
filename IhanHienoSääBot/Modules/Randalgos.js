6797968fe8ef242d5e533b6ca2134edd1f500f6d
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2026-03-08 20:25:22 +0200

    Update Randalgos.js

--- IhanHienoSääBot/Modules/Randalgos.js
+++ IhanHienoSääBot/Modules/Randalgos.js
@@ -7,23 +7,60 @@ import {retpath} from "../../Global_Modules/ESMfilepath.js"
 
 const pth = await retpath(import.meta.url);
 
-export class algos{
+export class bytestuff{
   async bitshift(input = []){ // Left & Right Side Bitshifting Methods
     return new Promise((r) => {
       const hex=Number.parseInt(input[0]),hexstr=hex.toString(16).toUpperCase(),intt=Number.parseInt(input[2])
+      if (isNaN(hex) || isNaN(intt)) r("Invalid numbers");
       input.map(dat => {
         if(/>>/g.test(dat)){
           var fin = hex >> intt,fin2=Number.parseInt(fin);
-          r(`Right Shift of:(0x${hexstr}) With Int:(${intt})\n\t\t\t\t Equals Decimal:(${fin}) & Hex:(0x${fin2.toString(16)})`);
+          r(`Right Shift of:(0x${hexstr}(Dec:${hex})) With Int:(${intt})\n\t\t\t\t\  Equals Hex:(0x${fin2.toString(16).toUpperCase()}(Dec:(${fin})))`);
+          hex = null,hexstr=null,intt=null;
+
         }
         else if(/<</g.test(dat)){
-          const fin = hex << intt,fin2=Number.parseInt(fin);
-          r(`Left Shift of:(0x${hexstr}) With Int:(${intt})\n\t\t\t\t Equals Decimal:(${fin}) & Hex:(0x${fin2.toString(16)})`);
+          var fin = hex << intt,fin2=Number.parseInt(fin);
+          r(`Left Shift of:(0x${hexstr}(Dec:${hex})) With Int:(${intt})\n\t\t\t\t\   Equals Hex:(0x${fin2.toString(16).toUpperCase()}(Dec:(${fin})))`);
         }
       })
     })
   }
+   async XOR(str,key){
+      return new Promise((res) => {
+        const b = Buffer.from(str)
+        for(let i=0;i<b.length;i++){
+          b[i] ^= key;
+      }
+          res(b.toString());
+      })
+    }
 
+  async and_or_not(input = []){
+    return new Promise((res) => {
+      var val = Number.parseInt(input[0]),val2 = Number.parseInt(input[2]);
+      switch(input[1]){
+        case "&": // AND
+          const and = val & val2;
+          res(`AND Mask of:(0x${val.toString(16).toUpperCase()}(Dec:${val})) With (0x${val2.toString(16).toUpperCase()}(Dec:${val2})\n\t\t\t\t\   Equals Hex:(0x${and.toString(16).toUpperCase()}(Dec:(${and})))`);
+          break; 
+        case "|": // OR
+          break;
+        case "~": // NOT
+          break;
+        }
+    })
+  }
+
+  async swap_endian(num){
+    num = num >>> 0;
+    var a = (((num >> 24) & 0xFF)|((num >> 8) & 0xFF00)| ((num << 8) & 0xFF0000)|((num << 24) & 0xFF000000))
+    a = a >>> 0;
+    return(a);
+  }
+}
+
+export class algos{
    async crc32(str,len,poly){
     return new Promise((res) => {
       exec(`'${pth}/crc32' ${str} ${len} ${poly}`,(er,stdout,stderr) => {
@@ -37,20 +74,10 @@ export class algos{
     })
   }
 
- async XOR(str,key){
-    return new Promise((res) => {
-      const b = Buffer.from(str)
-      for(let i=0;i<b.length;i++){
-        b[i] ^= key;
-    }
-        res(b.toString());
-    })
-  }
-
 async ucharprint(hex = [],parser){
   return new Promise((res) => {
   var unicode = [];
-
+    
   for(let i=0;i<hex.length;i++){
     if(!parser.test(hex[i]) || hex[0] > hex[1])
       res("Enter Hex Range in Valid Format...");
@@ -63,6 +90,8 @@ async ucharprint(hex = [],parser){
     }
   }
     const chars = unicode.slice(0,900).join(' ');
+    var chrs = 0;
+    chars.forEach(char => {chrs++})
     const remaining = unicode.slice(900).length;
     const result = `Chars Over Msg limit:(${remaining})\`\`\`\ ${chars.trim()}\n\`\`\``
     res(result);
